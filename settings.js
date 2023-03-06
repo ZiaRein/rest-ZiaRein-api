2a45f759f2eb11f2616b091c2e278f1b3acba288
ZiaRein <125527601+ZiaRein@users.noreply.github.com>
2023-03-06 14:56:38 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,7 +4,7 @@ global.creator = 'ZiaRein'// yourname
 global.MONGO_DB_URI = "mongodb+srv://smkxt7:angga.123@cluster0.hm6kawt.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
 global.your_email = "gwwpxk@gmail.com" //email
-global.email_password = "" //application password email
+global.email_password = "gwwpxk" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 5000
 global.loghandler = {
